44642d2d41585ad4d278391c3d074a717e7337d8
nomakun-des <121710889+nomakun-des@users.noreply.github.com>
2023-03-04 09:15:48 +0900

    Create include.js

---
+++ include.js
@@ -0,0 +1,112 @@
+var video_list = [  //year,month,day,name,img_no
+    ["23", "03", "05", "バレパンドクロッグ", 210]
+];
+
+window.onload = function () {
+    Nav();
+
+    const loader = document.getElementById('loader');
+    loader.classList.add('loaded');
+};
+
+function Nav() {
+    video_all = "";
+    recommend_url = "1EnJ4p3-W58";
+
+    for (let i = 0; i < video_list.length; i++) {
+        video_all +=
+                "<li><a href='" + video_list[i][0] + "_" + video_list[i][1] + "-" + video_list[i][2] + ".html'>" +
+                "    <img src='img/poke_" + video_list[i][4] + ".png'/>" +
+                "    <span>" + video_list[i][3] + " <font size='-2'>(20" + video_list[i][0] + "-" + video_list[i][1] + "-" + video_list[i][2] + ")</font></span>" +
+                "</a></li>";
+    }
+
+    nav_all = "<div class='menu_box'>" +
+            "<h2 onclick='Click_head(&quot;link&quot;)'>リンク</h2>" +
+            "<div id='link'>" +
+            "    <ul>" +
+            "        <li><a target='_blank' href='https://www.youtube.com/channel/UCvUSI-BrEgCY1nNOwosfifg'>" +
+            "                <img src='https://www.youtube.com/s/desktop/451d4225/img/favicon_96x96.png'/>" +
+            "                <span>YouTube(のーまです。【ゲーム実況】)</span>" +
+            "            </a></li>" +
+            "        <li><a target='_blank' href='https://twitter.com/nomakun_des'>" +
+            "                <img src='https://abs.twimg.com/favicons/twitter.2.ico'/>" +
+            "                <span>Twitter(@nomakun_des)</span>" +
+            "            </a></li>" +
+            "        <li><a target='_blank' href='https://www.instagram.com/nomakun_des/'>" +
+            "                <img src='https://static.cdninstagram.com/rsrc.php/v3/yt/r/30PrGfR3xhB.png'/>" +
+            "                <span>Instagram(nomakun_des)</span>" +
+            "            </a></li>" +
+            "        <li><a target='_blank' href='https://nomakun-des.fc2.net/'>" +
+            "                <img src='http://static.fc2.com/share/image/favicon.ico'/>" +
+            "                <span>FC2(のーまのぶろぐです。)</span>" +
+            "            </a></li>" +
+            "    </ul>" +
+            "</div>" +
+            "</div>" +
+            "    <div class='menu_box'>" +
+            "<h2 onclick='Click_head(&quot;tool&quot;)'>作成ツール</h2>" +
+            "<div id='tool'>" +
+            "    <ul>" +
+            "        <li><a href='https://nomakun-des.github.io/pokes-calc/'>" +
+            "                <img src='https://www.pokemon.co.jp/common/images/favi_official.ico'/>" +
+            "                <span>ステータス計算</span>" +
+            "            </a></li>" +
+            "        <li><a href='https://nomakun-des.github.io/types-check/'>" +
+            "                <img src='https://www.pokemon.co.jp/common/images/favi_official.ico'/>" +
+            "                <span>技範囲チェッカー</span>" +
+            "            </a></li>" +
+            "    </ul>" +
+            "</div>" +
+            "</div>" +
+            "    <div class='menu_box'>" +
+            "<h2 onclick='Click_head(&quot;video&quot;)'><a class='no-deco' href='video.html'>動画用記事</a></h2>" +
+            "<div id='video' style='display:none;'>" +
+            "    <ul>" +
+            video_all +
+            "    </ul>" +
+            "</div>" +
+            "</div>" +
+            "    <div class='menu_box'>" +
+            "<h2 onclick='Click_head(&quot;game&quot;)'>ゲームタイトル</h2>" +
+            "<div id='game' style='display:none;'>" +
+            "    <ul>" +
+            "        <li><a target='_blank' href='https://www.pokemon.co.jp/ex/sv/ja/'>" +
+            "                <img src='https://www.pokemon.co.jp/common/images/favi_official.ico'/>" +
+            "                <span>ポケットモンスターSV</span>" +
+            "            </a></li>" +
+            "        <li><a target='_blank' href='https://www.nintendo.co.jp/switch/av5ja/index.html'>" +
+            "                <img src='https://pbs.twimg.com/profile_images/1362173855100334082/sg2p-G27_400x400.jpg'/>" +
+            "                <span>スプラトゥーン3</span>" +
+            "            </a></li>" +
+            "    </ul>" +
+            "</div>" +
+            "</div>" +
+            "    <div class='menu_box'>" +
+            "<h2 onclick='Click_head(&quot;useful&quot;)'>よく使ってるサイト</h2>" +
+            "<div id='useful' style='display:none;'>" +
+            "    <ul>" +
+            "        <li><a target='_blank' href='https://www.youtube.com/'>" +
+            "                <img src='https://www.youtube.com/s/desktop/451d4225/img/favicon_96x96.png'/>" +
+            "                <span>YouTube</span>" +
+            "            </a></li>" +
+            "        <li><a target='_blank' href='https://yakkun.com/'>" +
+            "                <img src='https://img.yakkun.com/favicon-32x32.png'/>" +
+            "                <span>ポケモン徹底攻略</span>" +
+            "            </a></li>" +
+            "        <li><a target='_blank' href='https://sv.pokesol.com/calc'>" +
+            "                <img src='https://sv.pokesol.com/favicon-32x32.png'/>" +
+            "                <span>ダメージ計算</span>" +
+            "            </a></li>" +
+            "    </ul>" +
+            "</div>" +
+            "</div>";
+    recommend_all = "<br><br>"+
+            "<h1>おすすめ動画</h1>" +
+            "    <div id='cnt_inside'>" +
+            "        <iframe width='500' height='280' src='https://www.youtube.com/embed/" + recommend_url + "'></iframe>" +
+            "    </div>";
+
+    document.getElementById("inc_nav").innerHTML = nav_all;
+    document.getElementById("inc_recommend").innerHTML = recommend_all;
+}
